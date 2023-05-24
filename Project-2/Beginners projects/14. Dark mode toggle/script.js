b56e9f08dc6be857d9dcdaceb688d700ad1f895a
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-25 00:04:35 +0530

    completed

--- Project-2/Beginners projects/14. Dark mode toggle/script.js
+++ Project-2/Beginners projects/14. Dark mode toggle/script.js
@@ -1,18 +1,22 @@
 const inputE1 = document.querySelector(".input");
 
 const bodyE1 = document.querySelector("body")
-console.log(inputE1.checked);
-
+inputE1.checked = JSON.parse(localStorage.getItem("mode"))
 function updateBody() {
 
     if (inputE1.checked) {
         bodyE1.style.background = "black"
-    }else{
-        bodyE1.style.background ="white"
+    } else {
+        bodyE1.style.background = "white"
     }
 
 }
 
-inputE1.addEventListener("click", ()=>{
-    updateBody()
+inputE1.addEventListener("input", () => {
+    updateBody();
+    updateLocalstorage();
 })
+
+function updateLocalstorage() {
+    localStorage.setItem("mode", JSON.stringify(inputE1.checked));
+}
